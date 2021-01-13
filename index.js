96ca09eabef76d24d318111403305b6ec001e4b4
Starlasers <starlasers12@gmail.com>
2021-01-12 20:02:46 -0800

    add code

---
+++ index.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+
+
+
+
+client.on('ready', () => {
+  client.user.setActivity('Orangius Empire')
+});
+
+
+
+
+
+
+
+
+client.login('NjQ3OTU0OTQwMTY1MjI2NTA3.XdnNEA._w61FX4mM_n3JRVSnlw2jUMGYCw');
